13e9dd6c7f94133d671e871542f2bfb6abc475a2
franck <franckdepicardie@gmail.com>
2019-12-09 14:43:34 +0100

    c

--- release.sh
+++ release.sh
@@ -1,4 +1,11 @@
 echo "!!!!!!!!!!!!!!!!!!!!! Starting release.sh"
+echo "PART I"
 jar -xvf jenkins.war
-cp -rf ./jenkins/eyspecial/. .jenkins/
+echo "PART II"
+ls -la
+echo "PART III"
+cp -rf ./eyspecial/. .jenkins/
+echo "PART IV"
+ls -la .jenkins/
+echo "PART V"
 echo "!!!!!!!!!!!!!!!!!!!!! Finished release.sh"
