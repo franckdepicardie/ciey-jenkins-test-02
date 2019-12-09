d03aac9e992b8e009b054bff69187d40335952d8
franck <franckdepicardie@gmail.com>
2019-12-09 15:01:21 +0100

    c

--- release.sh
+++ release.sh
@@ -1,10 +1,13 @@
 echo "!!!!!!!!!!!!!!!!!!!!! Starting release.sh"
 echo "PART I"
 jar -xvf jenkins.war
-echo "PART II"
-ls -la .jenkins/jobs/
+echo "PART II-a"
+ls -la .jenkins
+echo "PART II-b"
+chmod -R 775 .jenkins
+echo "PART II-c"
+ls -la .jenkins/jobs
 echo "PART III"
 echo "-->copying jobs..."
-chmod -R 766 .jenkins/jobs/
 cp -rf ./eyspecial/jobs/. .jenkins/jobs/.
 echo "!!!!!!!!!!!!!!!!!!!!! Finished release.sh"
